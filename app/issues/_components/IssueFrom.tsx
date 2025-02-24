177c5e5147501b3dcde07f25097700e45df00c02
Carlos Gonzalez <uno1425@gmail.com>
2025-02-24 05:29:19 -0600

    Built an API for updating issues

--- app/issues/_components/IssueFrom.tsx
+++ app/issues/_components/IssueFrom.tsx
@@ -9,7 +9,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { InfoCircledIcon } from "@radix-ui/react-icons";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
@@ -19,7 +19,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 }); // Don't render this component on the server
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 // interface Props {
 //   issue?: Issue;
@@ -33,7 +33,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   }); //we destructure the obj to grab the register function
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
