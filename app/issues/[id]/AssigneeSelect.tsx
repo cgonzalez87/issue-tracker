95f1d201a997594d36392e9dfc7be1d4edc2ec54
Carlos Gonzalez <uno1425@gmail.com>
2025-03-15 11:45:49 -0500

    Fetching data using React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,19 +1,39 @@
 "use client";
 
+import { Skeleton } from "@/app/components";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import delay from "delay";
 
 const AssigneeSelect = () => {
-  const [users, setUser] = useState<User[]>([]);
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data: Users } = await axios.get<User[]>("/api/users");
-      setUser(Users);
-    };
-    fetchUsers();
-  }, []);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const response = await axios.get<User[]>("/api/users");
+      return response.data;
+    },
+    staleTime: 1000 * 60, // 1 minute
+    retry: 3,
+  });
+
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
+
+  // const [users, setUser] = useState<User[]>([]);
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     const { data: Users } = await axios.get<User[]>("/api/users");
+  //     setUser(Users);
+  //   };
+  //   fetchUsers();
+  // }, []);
 
   return (
     <Select.Root>
@@ -21,7 +41,7 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
